f2171126162a86faa632f12433401150308728d9
79316758+koudashinya@users.noreply.github.com <79316758+koudashinya@users.noreply.github.com>
2022-02-12 17:21:13 +0900

    #portfolio画面追加

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-class  Home extends React.Component {
-  render() {
-    return (
-        <a>お知らせに載せてる記事の最新から二つ載せるようにする(未実装)</a>
-    );
-  }
-}
+const Home = () => {
+  return <>this is react home.</>;
+};
  
 export default Home;
